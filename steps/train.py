ff6b83451a8e50d51bb509870bacfd981ea565cc
kuhlenh <kuhlenh>
2022-06-23 03:26:51 +0000

    changed to decisiontree

--- steps/train.py
+++ steps/train.py
@@ -2,6 +2,6 @@ def estimator_fn():
     """Returns an UNFITTED estimator that contains fit() and predict() method.
     Their input and output signatures should be compatible with sklearn estimators.
     """
-    from sklearn.linear_model import SGDRegressor
+    from sklearn.tree import DecisionTreeRegressor
 
-    return SGDRegressor(random_state=42)
+    return DecisionTreeRegressor(criterion="mse", max_depth=12, random_state=641401879)
