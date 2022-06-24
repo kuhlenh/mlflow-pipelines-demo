a65faedd04c972cfd1e0bcb25a4186ccf441304c
kuhlenh <kuhlenh>
2022-06-24 16:09:40 +0000

    demo start

--- notebooks/databricks.py
+++ notebooks/databricks.py
@@ -1,39 +1,8 @@
 # Databricks notebook source
-# Pipeline and steps are the main concepts one interacts with MLflow Pipelines.
-# To create a Pipeline, simply do p = Pipeline() within this pipeline template directory.
 from mlflow.pipelines import Pipeline
 
 p = Pipeline(profile="databricks")
 
 # COMMAND ----------
 
-p.clean()
-
-# COMMAND ----------
-
-p.inspect()
-
-# COMMAND ----------
-
-p.run("ingest")
-
-# COMMAND ----------
-
 p.run("train")
-
-# COMMAND ----------
-
-p.run("evaluate")
-
-# COMMAND ----------
-
-#p.run("register")
-
-# COMMAND ----------
-
-# test_data = p.get_artifact("test_data")
-# test_data.describe()
-
-# COMMAND ----------
-
-
