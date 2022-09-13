cbcc96bda872a9d986fbcec0ce494e5753746759
kuhlenh <kuhlenh>
2022-09-13 14:48:30 +0000

    automl

--- steps/train.py
+++ steps/train.py
@@ -12,6 +12,24 @@ def estimator_fn():
     The estimator's input and output signatures should be compatible with scikit-learn
     estimators.
     """
-    from sklearn.linear_model import SGDRegressor
+    from sklearn.linear_model import LogisticRegression
 
-    return SGDRegressor(random_state=42)
+    with mlflow.start_run(experiment_id="2555133066289073", run_name="logistic_regression") as mlflow_run:
+        # AutoML balanced the data internally and use _automl_sample_weight_0dbb to calibrate the probability distribution
+        sklr_sample_weight = X_train.loc[:, "_automl_sample_weight_0dbb"].to_numpy()
+
+        model.fit(X_train, y_train, classifier__sample_weight=sklr_sample_weight)
+
+        # Log metrics for the training set
+        sklr_training_metrics = mlflow.sklearn.eval_and_log_metrics(model, X_train, y_train, prefix="training_", pos_label=1, sample_weight=sklr_sample_weight)
+
+        # Log metrics for the validation set
+        sklr_val_metrics = mlflow.sklearn.eval_and_log_metrics(model, X_val, y_val, prefix="val_", pos_label=1)
+
+        # Log metrics for the test set
+        sklr_test_metrics = mlflow.sklearn.eval_and_log_metrics(model, X_test, y_test, prefix="test_", pos_label=1)
+
+        # Display the logged metrics
+        sklr_val_metrics = {k.replace("val_", ""): v for k, v in sklr_val_metrics.items()}
+        sklr_test_metrics = {k.replace("test_", ""): v for k, v in sklr_test_metrics.items()}
+        display(pd.DataFrame([sklr_val_metrics, sklr_test_metrics], index=["validation", "test"]))
--- steps/transform.py
+++ steps/transform.py
@@ -5,25 +5,47 @@ This module defines the following routines used by the 'transform' step of the r
   to the estimator during model inference.
 """
 
-from pandas import DataFrame
+import mlflow
+import databricks.automl_runtime
+from databricks import feature_store
+from databricks.feature_store import FeatureLookup
+from pandas import Timestamp
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder, StandardScaler, FunctionTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from databricks.automl_runtime.sklearn import DatetimeImputer
+from databricks.automl_runtime.sklearn import TimestampTransformer
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+import sklearn
+from sklearn import set_config
+from sklearn.pipeline import Pipeline
+
+fs = feature_store.FeatureStoreClient()
+target_col = "clicked"
+feature_table="ad_kasey.online_session"
 
 
 def calculate_features(df: DataFrame):
-    """
-    Extend the input dataframe with pickup day of week and hour, and trip duration.
-    Drop the now-unneeded pickup datetime and dropoff datetime columns.
-    """
-    df["pickup_dow"] = df["tpep_pickup_datetime"].dt.dayofweek
-    df["pickup_hour"] = df["tpep_pickup_datetime"].dt.hour
-    trip_duration = (
-            df["tpep_dropoff_datetime"] - df["tpep_pickup_datetime"]
+    online_feature_lookups = [
+    FeatureLookup( 
+      table_name = feature_table,
+      feature_names = ["age", "num_purchases_last_6_months"],
+      lookup_key = ["user_id" ],
+      timestamp_lookup_key=["impression_timestamp"]
     )
-    df["trip_duration"] = trip_duration.map(lambda x: x.total_seconds() / 60)
-    df.drop(columns=["tpep_pickup_datetime", "tpep_dropoff_datetime"], inplace=True)
-    return df
+    ]
+    raw = spark.table("ad_kasey.inference_silver").select("user_id", "impression_timestamp", "session_id", "clicked")
+    
+    training_set = fs.create_training_set(raw,
+    feature_lookups = online_feature_lookups,
+    label = "clicked"
+    )
+    training_df = training_set.load_df()
+
+    
+    return training_df.toPandas()
 
 
 def transformer_fn():
@@ -32,33 +54,37 @@ def transformer_fn():
     The transformer's input and output signatures should be compatible with scikit-learn
     transformers.
     """
-    return Pipeline(
-        steps=[
-            (
-                "calculate_time_and_duration_features",
-                FunctionTransformer(calculate_features, feature_names_out="one-to-one"),
-            ),
-            (
-                "encoder",
-                ColumnTransformer(
-                    transformers=[
-                        (
-                            "hour_encoder",
-                            OneHotEncoder(categories="auto", sparse=False),
-                            ["pickup_hour"],
-                        ),
-                        (
-                            "day_encoder",
-                            OneHotEncoder(categories="auto", sparse=False),
-                            ["pickup_dow"],
-                        ),
-                        (
-                            "std_scaler",
-                            StandardScaler(),
-                            ["trip_distance", "trip_duration"],
-                        ),
-                    ]
-                ),
-            ),
-        ]
-    )
+    imputers = {
+      "impression_timestamp": DatetimeImputer(),
+    }
+
+    datetime_transformers = []
+
+    for col in ["impression_timestamp"]:
+        ohe_transformer = ColumnTransformer(
+            [("ohe", OneHotEncoder(sparse=False, handle_unknown="ignore"), [TimestampTransformer.HOUR_COLUMN_INDEX])],
+            remainder="passthrough")
+        timestamp_preprocessor = Pipeline([
+            (f"impute_{col}", imputers[col]),
+            (f"transform_{col}", TimestampTransformer()),
+            (f"onehot_encode_{col}", ohe_transformer),
+            (f"standardize_{col}", StandardScaler()),
+        ])
+        datetime_transformers.append((f"timestamp_{col}", timestamp_preprocessor, [col]))
+        transformers = datetime_transformers
+
+        preprocessor = ColumnTransformer(transformers, remainder="passthrough", sparse_threshold=0)
+        
+        sklr_classifier = LogisticRegression(
+          C=2.30556906175862,
+          penalty="l1",
+          solver="saga",
+          random_state=523525670,
+        )
+    
+    return Pipeline([
+          ("column_selector", col_selector),
+          ("preprocessor", preprocessor),
+          ("classifier", sklr_classifier),
+      ])
+        
